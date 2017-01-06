3fe927b68b25d15e013fba6ad43719790609eddd
Tyson Gern <tgern@pivotal.io>
2017-01-06 16:54:37 -0700

    Extract microservice

--- components/cf-support/src/main/java/org.superbiz.cloudfoundry/ServiceCredentials.java
+++ components/cf-support/src/main/java/org.superbiz.cloudfoundry/ServiceCredentials.java
@@ -1,4 +1,4 @@
-package org.superbiz.moviefun;
+package org.superbiz.cloudfoundry;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
